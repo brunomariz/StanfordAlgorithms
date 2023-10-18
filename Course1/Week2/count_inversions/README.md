202f52a9d683ea69a444ddec37dd5e2067987298
Bruno Mariz <bmarizot@gmail.com>
2023-10-18 03:05:06 -0300

    initial assignment code

---
+++ Course1/Week2/count_inversions/README.md
@@ -0,0 +1,11 @@
+<!-- gh-action-output -->
+## Program output
+```bash
+$ make
+$ ./a.out 
+Input:
+[7, 1, 6, 8, 3, 5, 9, 56, 2, 24]
+Output:
+[1, 2, 3, 5, 6, 7, 8, 9, 24, 56]
+```
+<!-- gh-action-output end -->
---
+++ Course1/Week2/count_inversions/count_inversions.c
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define INPUT_LEN 6
+
+void print_int_array(int *input, int length);
+int floor(double x);
+int ceil(double x);
+int *merge(int *a, int len_a, int *b, int len_b);
+int *merge_sort(int *input, int len);
+int count_inversions(int **array, int length);
+int count_split_inversions(int **array, int length);
+
+int main() {
+    int *input = malloc(sizeof(int) * INPUT_LEN);
+    input[0] = 2;
+    input[1] = 3;
+    input[2] = 8;
+    input[3] = 6;
+    input[4] = 1;
+    input[5] = 5;
+    // input[6] = 9;
+    // input[7] = 56;
+    // input[8] = 2;
+    // input[9] = 24;
+    // input[10] = 10;
+    // input[11] = 4;
+    // input[12] = 12;
+    // input[13] = 31;
+    // input[14] = 20;
+
+    printf("Input:\n");
+    print_int_array(input, INPUT_LEN);
+
+    int count = count_inversions(&input, INPUT_LEN);
+    print_int_array(input, INPUT_LEN);
+
+    // int *output = merge_sort(input, INPUT_LEN);
+
+    printf("Number of inversions: %d\n", count);
+
+    return 0;
+}
+
+int count_inversions(int **array, int length) {
+    if (length == 1) {
+        return 0;
+    }
+
+    // Divide array in two halves
+    int len_first_half = floor((double)length / 2.0);
+    int len_second_half = ceil((double)length / 2.0);
+
+    int *first_half = *array;
+
+    int *second_half = &((*array)[len_first_half]);
+
+    // Recursevely count inversions in each half
+    int x = count_inversions(&first_half, len_first_half);
+    int y = count_inversions(&second_half, len_second_half);
+    // Count inversions that happen between each half
+    printf("before\n");
+    print_int_array(first_half, len_first_half);
+    int z = count_split_inversions(array, length);
+    printf("after\n");
+    print_int_array(first_half, len_first_half);
+
+    return x + y + z;
+}
+
+int count_split_inversions(int **array, int length) {
+    int *sorted = malloc((length) * sizeof(int));
+    int split_inversions_count = 0;
+
+    // Divide array in two halves
+    int len_first_half = floor((double)length / 2.0);
+    int len_second_half = ceil((double)length / 2.0);
+
+    int *first_half = malloc(sizeof(int) * len_first_half);
+    for (size_t i = 0; i < len_first_half; i++) {
+        first_half[i] = (*array)[i];
+    }
+
+    int *second_half = malloc(sizeof(int) * len_second_half);
+    for (size_t i = 0; i < len_second_half; i++) {
+        second_half[i] = (*array)[len_first_half + i];
+    }
+
+    int i = 0;
+    int j = 0;
+    for (size_t k = 0; k < len_first_half + len_second_half; k++) {
+        if (((first_half[i] <= second_half[j]) && i < len_first_half) ||
+            j >= len_second_half) {
+            sorted[k] = first_half[i];
+            i++;
+        } else {
+            sorted[k] = second_half[j];
+            j++;
+            split_inversions_count += len_first_half - i;
+        }
+    }
+
+    for (size_t i = 0; i < length; i++) {
+        (*array)[i] = (sorted)[i];
+    }
+
+    return split_inversions_count;
+}
+
+int *merge(int *a, int len_a, int *b, int len_b) {
+    int *c = malloc((len_a + len_b) * sizeof(int));
+    int i = 0;
+    int j = 0;
+    for (size_t k = 0; k < len_a + len_b; k++) {
+        if (((a[i] <= b[j]) && i < len_a) || j >= len_b) {
+            c[k] = a[i];
+            i++;
+        } else {
+            c[k] = b[j];
+            j++;
+        }
+    }
+    return c;
+}
+
+int *merge_sort(int *input, int len) {
+    int len_a = len / 2;
+    int *a = malloc(len_a * sizeof(int));
+
+    for (size_t i = 0; i < len_a; i++) {
+        a[i] = input[i];
+    }
+
+    if (len_a > 1) {
+        a = merge_sort(a, len_a);
+    }
+
+    int len_b = len / 2 + len % 2;
+    int *b = malloc(len_b * sizeof(int));
+
+    for (size_t i = 0; i < len_b; i++) {
+        b[i] = input[len - len_b + i];
+    }
+
+    if (len_b > 1) {
+        b = merge_sort(b, len_b);
+    }
+
+    return merge(a, len_a, b, len_b);
+}
+
+void print_int_array(int *input, int length) {
+    printf("[");
+    for (unsigned short i = 0; i < length; i++) {
+        printf("%d", input[i]);
+        if (i < length - 1) {
+            printf(", ");
+        }
+    }
+    printf("]\n");
+}
+
+int floor(double x) {
+    int x_round = (int)x;
+    if (x_round - x <= 0) {
+        return x_round;
+    }
+    return x_round - 1;
+}
+
+int ceil(double x) {
+    int x_round = (int)x;
+    if (x_round - x >= 0) {
+        return x_round;
+    }
+    return x_round + 1;
+}
