0ed6974970c4e7b1280207f08874da4826a65e6c
Bruno Mariz <bmarizot@gmail.com>
2023-10-26 01:03:25 -0300

    exclude submodule from dir list

--- scripts/update_all_readmes.sh
+++ scripts/update_all_readmes.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 dirs=$(find . -type f -name 'Makefile' -not -path "./c_structures/*" | sed -r 's|/[^/]+$|/|' |sort |uniq)
-echo "${dirs}"
-# for dir in $dirs; do
-#     echo "--> Running action on $dir"
-#     sh scripts/compile_run_update_readme.sh $dir
-# done
+
+for dir in $dirs; do
+    echo "--> Running action on $dir"
+    sh scripts/compile_run_update_readme.sh $dir
+done
 
